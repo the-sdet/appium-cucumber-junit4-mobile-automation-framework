4927bf1b9ca4ed62ca8f9aaaf7d4aeea454eefce
Pabitra <pabitra.swain.officework@gmail.com>
2024-04-21 01:15:58 +0530

    test-automation-toolkit version update. Enhancements in Engine related to remote execution

--- src/main/java/utils/CommonUtils.java
+++ src/main/java/utils/CommonUtils.java
@@ -14,35 +14,6 @@ import static io.github.the_sdet.cucumber.CucumberUtils.*;
  * @author Pabitra Swain (contact.the.sdet@gmail.com)
  */
 public class CommonUtils {
-
-    /**
-     * Extracts the feature name from the given Scenario object.
-     *
-     * @param scenario        The Scenario object from Cucumber.
-     * @param withPackageName A boolean indicating whether to include package name along with feature name.
-     * @return The feature name extracted from the Scenario.
-     * @author Pabitra Swain (contact.the.sdet@gmail.com)
-     */
-    public static String getFeatureNameFromScenario(Scenario scenario, boolean withPackageName) {
-        String uri = scenario.getUri().toString();
-        String featureName, packageName;
-        if (uri.startsWith("file")) {
-            String[] test = uri.split("/");
-            int size = test.length;
-            featureName = test[size - 1].split("\\.")[0];
-            packageName = test[size - 2];
-        } else {
-            String[] test = uri.split("/");
-            featureName = test[1].split("\\.")[0];
-            packageName = test[0].split(":")[1];
-        }
-        Log.info("Feature: " + featureName);
-        Log.info("Package: " + packageName);
-        if (withPackageName)
-            return packageName + " - " + featureName;
-        else return featureName;
-    }
-
     /**
      * Attaches screenshot to the Cucumber report based on the configuration.
      *
